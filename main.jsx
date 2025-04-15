964c13fdd0cbee4a4bfcd3320b75d169f332d9be
VladislavVik1 <Dakraman1232@ukr.net>
2025-04-15 16:27:59 +0300

    1

--- main.jsx
+++ main.jsx
@@ -1,7 +1,8 @@
+import App from './src/components/App.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './src/index.css'
-import App from './src/components/App'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
